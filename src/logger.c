e6a89c092d8e338961aceb30d1215188327de3b5
Muhammad Khubaib Umer <khubaibumer@live.com>
2022-07-16 00:49:13 +0500

    Indentation and minor changes

--- src/logger.c
+++ src/logger.c
@@ -10,17 +10,20 @@
 
 #define GENERATE_PRINT(info, fmt) \
 ({                       \
-	va_list temp, args;     \
+    va_list temp, args;     \
     va_start(temp, (fmt)); \
     va_copy(args, temp); \
-	size_t len = vsnprintf(NULL, 0, (fmt), temp); \
-	va_end(temp);           \
-	(info)->log_line = calloc(len + 1, sizeof(char)); \
-	vsnprintf((info)->log_line, len, (fmt), args);  \
-	va_end(args);\
+    size_t len = vsnprintf(NULL, 0, (fmt), temp); \
+    va_end(temp);           \
+    (info)->log_line = calloc(len + 1, sizeof(char)); \
+    vsnprintf((info)->log_line, len, (fmt), args);  \
+    va_end(args);\
 })
 
+#define GET_PRIVATE(x) (x)->private
+
 /* Forward Declarations */
+typedef struct log_info log_info_t;
 void print_trace(const char *fn, int ln, const char *fmt, ...);
 void print_debug(const char *fn, int ln, const char *fmt, ...);
 void print_info(const char *fn, int ln, const char *fmt, ...);
@@ -29,29 +32,41 @@ void print_error(const char *fn, int ln, const char *fmt, ...);
 bool change_log_level(kLogLevel level);
 void change_file(void);
 void *logging_thread(void *args);
+bool queue_lock(void);
+void queue_unlock(void);
+bool push_to_queue(log_info_t *);
+log_info_t *pop_from_queue(void);
 
 typedef struct log_info {
-  tid_t tid;
-  kLogLevel level;
-  int line;
-  char *func;
-  char *log_line;
+	tid_t tid;
+	kLogLevel level;
+	int line;
+	char *func;
+	char *log_line;
 
-  TAILQ_ENTRY(log_info) entries;
+	TAILQ_ENTRY(log_info) entries;
 } log_info_t;
 
 typedef struct {
-  logger_t *logger;
-  size_t current_file_size;
-  char *file_path;
-  size_t max_file_size;
-  FILE *logfile;
-  kLogLevel log_level;
-  bool is_running;
+	logger_t *logger;
+	size_t current_file_size;
+	char *file_path;
+	size_t max_file_size;
+	FILE *logfile;
+	kLogLevel log_level;
+	bool is_running;
+	pthread_t loggerThread;
 
-  pthread_t loggerThread;
-  TAILQ_HEAD(tailhead, log_info) head;
-  pthread_spinlock_t queue_lock;
+	struct {
+		TAILQ_HEAD(tailhead, log_info) head;
+		pthread_spinlock_t queue_lock;
+	} private;
+
+	bool (*lock)(void);
+	void (*unlock)(void);
+	bool (*push_back)(log_info_t *);
+	log_info_t *(*pop_front)(void);
+
 } loggerData_t;
 
 static logger_t logger = {
@@ -66,188 +81,200 @@ static logger_t logger = {
 };
 
 char *mkfile_name(bool startup) {
-  time_t timer;
-  char buffer[36] = {0};
-  struct tm *tm_info;
+	time_t timer;
+	char buffer[36] = {0};
+	struct tm *tm_info;
 
-  timer = time(NULL);
-  tm_info = localtime(&timer);
+	timer = time(NULL);
+	tm_info = localtime(&timer);
 
-  size_t bytes = strftime(buffer, 36, "%Y_%m_%d_%H_%M_%S", tm_info);
-  char *time_stamp = calloc(32 + bytes, sizeof(char));
-  if (startup) {
-	sprintf(time_stamp, "LogFile_%s_startup.log", buffer);
-  } else {
-	sprintf(time_stamp, "LogFile_%s.log", buffer);
-  }
-  return time_stamp;
+	size_t bytes = strftime(buffer, 36, "%Y_%m_%d_%H_%M_%S", tm_info);
+	char *time_stamp = calloc(32 + bytes, sizeof(char));
+	if (startup) {
+		sprintf(time_stamp, "LogFile_%s_startup.log", buffer);
+	} else {
+		sprintf(time_stamp, "LogFile_%s.log", buffer);
+	}
+	return time_stamp;
 }
 
 static loggerData_t *this = NULL;
 
 logger_t *create_logger(const char *path) {
-  if (this == NULL) {
-	this = calloc(1, sizeof(loggerData_t));
-	this->logger = &logger;
-	pthread_spin_init(&this->queue_lock, 0);
-	this->file_path = strdup(path);
-  }
+	if (this == NULL) {
+		this = calloc(1, sizeof(loggerData_t));
+		this->logger = &logger;
+		pthread_spin_init(&GET_PRIVATE(this).queue_lock, 0);
+		this->file_path = strdup(path);
+		this->lock = &queue_lock;
+		this->unlock = &queue_unlock;
+		this->push_back = &push_to_queue;
+		this->pop_front = &pop_from_queue;
+	}
 
-  assert(chdir(this->file_path) == 0);
-  this->is_running = true;
-  char *file = mkfile_name(true);
-  this->logfile = fopen(file, "w+");
-  TAILQ_INIT(&this->head);
-  free(file);
+	assert(chdir(this->file_path) == 0);
+	this->is_running = true;
+	char *file = mkfile_name(true);
+	this->logfile = fopen(file, "w+");
+	TAILQ_INIT(&GET_PRIVATE(this).head);
+	free(file);
 
-  pthread_create(&this->loggerThread, NULL, logging_thread, NULL);
-  pthread_detach(this->loggerThread);
+	pthread_create(&this->loggerThread, NULL, logging_thread, NULL);
+	pthread_detach(this->loggerThread);
 }
 
 bool set_max_file_size(size_t max) {
-  if (this == NULL)
-	return false;
+	if (this == NULL)
+		return false;
 
-  this->max_file_size = max;
-  return true;
+	this->max_file_size = max;
+	return true;
 }
 
 bool change_log_level(kLogLevel level) {
-  this->log_level = level;
+	this->log_level = level;
 }
 
 logger_t *get_logger(void) {
-  return this->logger;
+	return this->logger;
+}
+
+bool queue_lock(void) {
+	pthread_spin_lock(&GET_PRIVATE(this).queue_lock);
+}
+
+void queue_unlock(void) {
+	pthread_spin_unlock(&GET_PRIVATE(this).queue_lock);
 }
 
 bool close_logger(void) {
-  this->is_running = false;
-  fclose(this->logfile);
-  free(this->file_path);
-  free(this);
+	this->is_running = false;
+	fclose(this->logfile);
+	free(this->file_path);
+	free(this);
 }
 
 log_info_t *prepare_data(const char *fn, const int ln, kLogLevel level) {
-  log_info_t *info = calloc(1, sizeof(log_info_t));
-  info->level = level;
-  info->tid = syscall(SYS_gettid);
-  info->line = ln;
-  info->func = strdup(fn);
-  return info;
+	log_info_t *info = calloc(1, sizeof(log_info_t));
+	info->level = level;
+	info->tid = syscall(SYS_gettid);
+	info->line = ln;
+	info->func = strdup(fn);
+	return info;
 }
 
 bool push_to_queue(log_info_t *node) {
-  pthread_spin_lock(&this->queue_lock);
-  TAILQ_INSERT_TAIL(&this->head, node, entries);
-  pthread_spin_unlock(&this->queue_lock);
+	this->lock();
+	TAILQ_INSERT_TAIL(&GET_PRIVATE(this).head, node, entries);
+	this->unlock();
 }
 
 log_info_t *pop_from_queue() {
-  pthread_spin_lock(&this->queue_lock);
-  log_info_t *msg = this->head.tqh_first;
-  TAILQ_REMOVE(&this->head, this->head.tqh_first, entries);
-  pthread_spin_unlock(&this->queue_lock);
-  return msg;
+	this->lock();
+	log_info_t *msg = GET_PRIVATE(this).head.tqh_first;
+	TAILQ_REMOVE(&GET_PRIVATE(this).head, GET_PRIVATE(this).head.tqh_first, entries);
+	this->unlock();
+	return msg;
 }
 
 void print_trace(const char *fn, int ln, const char *fmt, ...) {
-  return_if(this->log_level >= LOG_TRACE,);
+	return_if(this->log_level >= LOG_TRACE,);
 
-  log_info_t *info = prepare_data(fn, ln, LOG_TRACE);
-  GENERATE_PRINT(info, fmt);
-  push_to_queue(info);
+	log_info_t *info = prepare_data(fn, ln, LOG_TRACE);
+	GENERATE_PRINT(info, fmt);
+	this->push_back(info);
 }
 
 void print_debug(const char *fn, int ln, const char *fmt, ...) {
-  return_if(this->log_level >= LOG_DEBUG,);
+	return_if(this->log_level >= LOG_DEBUG,);
 
-  log_info_t *info = prepare_data(fn, ln, LOG_DEBUG);
-  GENERATE_PRINT(info, fmt);
-  push_to_queue(info);
+	log_info_t *info = prepare_data(fn, ln, LOG_DEBUG);
+	GENERATE_PRINT(info, fmt);
+	this->push_back(info);
 }
 
 void print_info(const char *fn, int ln, const char *fmt, ...) {
-  return_if(this->log_level >= LOG_INFO,);
+	return_if(this->log_level >= LOG_INFO,);
 
-  log_info_t *info = prepare_data(fn, ln, LOG_INFO);
-  GENERATE_PRINT(info, fmt);
-  push_to_queue(info);
+	log_info_t *info = prepare_data(fn, ln, LOG_INFO);
+	GENERATE_PRINT(info, fmt);
+	this->push_back(info);
 }
 
 void print_warn(const char *fn, int ln, const char *fmt, ...) {
-  return_if(this->log_level >= LOG_WARN,);
+	return_if(this->log_level >= LOG_WARN,);
 
-  log_info_t *info = prepare_data(fn, ln, LOG_WARN);
-  GENERATE_PRINT(info, fmt);
-
-  push_to_queue(info);
+	log_info_t *info = prepare_data(fn, ln, LOG_WARN);
+	GENERATE_PRINT(info, fmt);
+	this->push_back(info);
 }
 
 void print_error(const char *fn, int ln, const char *fmt, ...) {
-  return_if(this->log_level >= LOG_ERROR,);
+	return_if(this->log_level >= LOG_ERROR,);
 
-  log_info_t *info = prepare_data(fn, ln, LOG_ERROR);
-  GENERATE_PRINT(info, fmt);
-  push_to_queue(info);
+	log_info_t *info = prepare_data(fn, ln, LOG_ERROR);
+	GENERATE_PRINT(info, fmt);
+	this->push_back(info);
 }
 
 const char *get_log_str(kLogLevel level) {
-  switch (level) {
-	case LOG_TRACE: return "TRACE";
-	case LOG_DEBUG: return "DEBUG";
-	case LOG_INFO: return "INFO";
-	case LOG_WARN: return "WARN";
-	case LOG_ERROR: return "ERROR";
-	default: return "UNKNOWN";
-  }
+	switch (level) {
+		case LOG_TRACE: return "TRACE";
+		case LOG_DEBUG: return "DEBUG";
+		case LOG_INFO: return "INFO";
+		case LOG_WARN: return "WARN";
+		case LOG_ERROR: return "ERROR";
+		default: return "UNKNOWN";
+	}
 }
 
 void change_file() {
-  printf("Changing File, current File Size %zu\n", this->current_file_size);
-  fclose(this->logfile);
-  char *filename = mkfile_name(false);
-  this->logfile = fopen(filename, "w+");
-  assert(this->logfile != NULL);
-  free(filename);
-  this->current_file_size = 0;
+	printf("Changing File, current File Size %zu\n", this->current_file_size);
+	fclose(this->logfile);
+	char *filename = mkfile_name(false);
+	this->logfile = fopen(filename, "w+");
+	assert(this->logfile != NULL);
+	free(filename);
+	this->current_file_size = 0;
 }
 
 char *get_current_time_stamp() {
-  time_t timer;
-  char buffer[36] = {0};
-  struct tm *tm_info;
+	time_t timer;
+	char buffer[48] = {0};
+	struct tm *tm_info;
 
-  timer = time(NULL);
-  tm_info = localtime(&timer);
+	timer = time(NULL);
+	tm_info = localtime(&timer);
 
-  strftime(buffer, 36, "%Y %m %d %H:%M:%S", tm_info);
-  return strdup(buffer);
+	strftime(buffer, sizeof(buffer), "%Y %m %d %H:%M:%S", tm_info);
+	return strdup(buffer);
 }
 
 void *logging_thread(void *args) {
-  (void)args;
-  const struct timespec sleep_time = {
-	  .tv_nsec = 10,
-	  .tv_sec = 0
-  };
+	(void)args;
+	const struct timespec sleep_time = {
+		.tv_nsec = 10,
+		.tv_sec = 0
+	};
 
-  while (this->is_running) {
-	while (TAILQ_EMPTY(&this->head)) {
-	  nanosleep(&sleep_time, NULL);
+	while (this->is_running) {
+		while (TAILQ_EMPTY(&GET_PRIVATE(this).head)) {
+			nanosleep(&sleep_time, NULL);
+		}
+
+		char *ts = get_current_time_stamp();
+		log_info_t *msg = this->pop_front();
+		this->current_file_size +=
+			fprintf(this->logfile, "%s [%lu] %s : %s:%d > %s", ts, msg->tid, get_log_str(msg->level),
+			        msg->func,
+			        msg->line, msg->log_line);
+		fflush(this->logfile);
+		free(msg);
+		free(ts);
+
+		if (this->current_file_size >= this->max_file_size) {
+			this->logger->cycle_file();
+		}
 	}
-
-	char *ts = get_current_time_stamp();
-	log_info_t *msg = pop_from_queue();
-	this->current_file_size += fprintf(this->logfile, "%s [%lu] %s : %s:%d > %s", ts, msg->tid, get_log_str(msg->level),
-									   msg->func,
-									   msg->line, msg->log_line);
-	fflush(this->logfile);
-	free(msg);
-	free(ts);
-
-	if (this->current_file_size >= this->max_file_size) {
-	  this->logger->cycle_file();
-	}
-  }
-  return NULL;
+	return NULL;
 }
