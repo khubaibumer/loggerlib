4398a3a8ea16fbdb10611ad8efe1af0aa0fdac66
khubaibumer <khubaibumer@gmail.com>
2022-08-05 22:32:01 +0500

    Changes in design

--- src/logger.c
+++ src/logger.c
@@ -7,12 +7,15 @@
 #include <sys/queue.h>
 #include <fcntl.h>
 #include <zlib.h>
+#include <semaphore.h>
+#include <dirent.h>
+#include <stdatomic.h>
 
 #define return_if(x, y) ({if (x) {return y;}})
 
 #define GENERATE_PRINT_FUNC(LEVEL) \
     void print_##LEVEL(const char *fn, int ln, const char *fmt, ...) { \
-        return_if(this->log_level > LEVEL, ); \
+        return_if(_this->log_level > LEVEL, ); \
         log_info_t *info = prepare_data(fn, ln, LEVEL); \
         va_list temp, args; \
         va_start(temp, fmt); \
@@ -22,7 +25,7 @@
         info->log_line = calloc(len + 2, sizeof(char)); \
         vsnprintf(info->log_line, len, fmt, args); \
         va_end(args); \
-        this->push_back(info); \
+        _this->push_back(info); \
     }
 
 #define DECLARE_PRINT_FUNC(LEVEL) \
@@ -30,7 +33,7 @@
 
 #define FUNC_PTR_PRINT(LEVEL) print_##LEVEL
 
-#define GET_PRIVATE(x) (x)->private
+#define GET_PRIVATE(x) (x)->p_data
 
 /* Forward Declarations */
 typedef struct log_info log_info_t;
@@ -47,8 +50,10 @@ bool queue_lock(void);
 void queue_unlock(void);
 bool push_to_queue(log_info_t *);
 log_info_t *pop_from_queue(void);
-bool ignite_compressor_routine(void);
-void *compress_log_file(void *);
+void* zip_file_routine(void *);
+void zip_all_uncompressed_files(void);
+//bool ignite_compressor_routine(void);
+//void *compress_log_file(void *);
 
 typedef struct log_info {
 	tid_t tid;
@@ -77,22 +82,28 @@ typedef struct {
 	struct {
 		TAILQ_HEAD(tailhead, log_info) head;
 		pthread_spinlock_t queue_lock;
-	} private;
+	} p_data;
+
+	struct {
+		sem_t sem;
+		pthread_t tid;
+		void (*send_signal) (void);
+		void (*wait_signal) (void);
+	} file_zip;
 
 	bool (*lock)(void);
 	void (*unlock)(void);
 	bool (*push_back)(log_info_t *);
 	log_info_t *(*pop_front)(void);
-	bool (*ignite_compressor)(void);
 
 } loggerData_t;
 
-typedef struct {
-	char *orig_filename;
-	size_t filename_len;
-	char *compressed_filename;
-	const char *file_path;
-} compressor_params_t;
+//typedef struct {
+//	char *orig_filename;
+//	size_t filename_len;
+//	char *compressed_filename;
+//	const char *file_path;
+//} compressor_params_t;
 
 static logger_t logger = {
 	.trace = &FUNC_PTR_PRINT(LOG_TRACE),
@@ -105,7 +116,7 @@ static logger_t logger = {
 	.cycle_file = &change_file
 };
 
-static loggerData_t *this = nullptr;
+static loggerData_t *_this = nullptr;
 
 GENERATE_PRINT_FUNC(LOG_TRACE)
 
@@ -116,6 +127,10 @@ GENERATE_PRINT_FUNC(LOG_INFO)
 GENERATE_PRINT_FUNC(LOG_WARN)
 
 GENERATE_PRINT_FUNC(LOG_ERROR)
+
+void send_job_signal(void) { sem_post(&_this->file_zip.sem); }
+
+void wait_for_job_signal(void) { sem_wait(&_this->file_zip.sem); }
 
 void logger_free(void *ptr) {
 	if (ptr) {
@@ -148,60 +163,65 @@ char *mkfile_name(bool startup, size_t *len) {
 }
 
 logger_t *create_logger(const char *service, const char *path) {
-	if (this == nullptr) {
-		this = calloc(1, sizeof(loggerData_t));
-		this->service = strdup(service);
-		this->logger = &logger;
-		pthread_spin_init(&GET_PRIVATE(this).queue_lock, 0);
-		pthread_spin_init(&this->workerLock, 0);
-		this->file_path = strdup(path);
-		this->lock = &queue_lock;
-		this->unlock = &queue_unlock;
-		this->push_back = &push_to_queue;
-		this->pop_front = &pop_from_queue;
-		this->ignite_compressor = &ignite_compressor_routine;
+	if (_this == nullptr) {
+		_this = calloc(1, sizeof(loggerData_t));
+		_this->service = strdup(service);
+		_this->logger = &logger;
+		pthread_spin_init(&GET_PRIVATE(_this).queue_lock, 0);
+		pthread_spin_init(&_this->workerLock, 0);
+		_this->file_path = strdup(path);
+		_this->lock = &queue_lock;
+		_this->unlock = &queue_unlock;
+		_this->push_back = &push_to_queue;
+		_this->pop_front = &pop_from_queue;
 	}
 
-	assert(chdir(this->file_path) == 0);
-	this->is_running = true;
-	this->filename = mkfile_name(true, &this->filename_len);
-	this->logfile = fopen(this->filename, "w+");
-	TAILQ_INIT(&GET_PRIVATE(this).head);
+	assert(sem_init(&_this->file_zip.sem, 0, 0) == 0);
+	_this->file_zip.send_signal = &send_job_signal;
+	_this->file_zip.wait_signal = &wait_for_job_signal;
+	pthread_create(&_this->file_zip.tid, nullptr, zip_file_routine, nullptr);
 
-	pthread_create(&this->loggerThread, nullptr, logging_thread, nullptr);
-	pthread_detach(this->loggerThread);
+	assert(chdir(_this->file_path) == 0);
+	_this->is_running = true;
+	_this->filename = mkfile_name(true, &_this->filename_len);
+	_this->logfile = fopen(_this->filename, "w+");
+	TAILQ_INIT(&GET_PRIVATE(_this).head);
+
+	pthread_create(&_this->loggerThread, nullptr, logging_thread, nullptr);
 }
 
 bool set_max_file_size(size_t max) {
-	if (this == nullptr)
+	if (_this == nullptr)
 		return false;
 
-	this->max_file_size = max;
+	_this->max_file_size = max;
 	return true;
 }
 
 bool change_log_level(kLogLevel level) {
-	this->log_level = level;
+	_this->log_level = level;
 }
 
 logger_t *get_logger(void) {
-	return this->logger;
+	return _this->logger;
 }
 
 bool queue_lock(void) {
-	pthread_spin_lock(&GET_PRIVATE(this).queue_lock);
+	pthread_spin_lock(&GET_PRIVATE(_this).queue_lock);
 }
 
 void queue_unlock(void) {
-	pthread_spin_unlock(&GET_PRIVATE(this).queue_lock);
+	pthread_spin_unlock(&GET_PRIVATE(_this).queue_lock);
 }
 
 bool close_logger(void) {
-	this->is_running = false;
-	fclose(this->logfile);
-	logger_free(this->file_path);
-	logger_free(this->service);
-	logger_free(this);
+	_this->is_running = false;
+	pthread_join(_this->file_zip.tid, nullptr);
+	pthread_join(_this->loggerThread, nullptr);
+	fclose(_this->logfile);
+	logger_free(_this->file_path);
+	logger_free(_this->service);
+	logger_free(_this);
 }
 
 log_info_t *prepare_data(const char *fn, int ln, kLogLevel level) {
@@ -214,16 +234,16 @@ log_info_t *prepare_data(const char *fn, int ln, kLogLevel level) {
 }
 
 bool push_to_queue(log_info_t *node) {
-	this->lock();
-	TAILQ_INSERT_TAIL(&GET_PRIVATE(this).head, node, entries);
-	this->unlock();
+	_this->lock();
+	TAILQ_INSERT_TAIL(&GET_PRIVATE(_this).head, node, entries);
+	_this->unlock();
 }
 
 log_info_t *pop_from_queue() {
-	this->lock();
-	log_info_t *msg = GET_PRIVATE(this).head.tqh_first;
-	TAILQ_REMOVE(&GET_PRIVATE(this).head, GET_PRIVATE(this).head.tqh_first, entries);
-	this->unlock();
+	_this->lock();
+	log_info_t *msg = GET_PRIVATE(_this).head.tqh_first;
+	TAILQ_REMOVE(&GET_PRIVATE(_this).head, GET_PRIVATE(_this).head.tqh_first, entries);
+	_this->unlock();
 	return msg;
 }
 
@@ -240,13 +260,14 @@ const char *get_log_str(kLogLevel level) {
 }
 
 void change_file() {
-	printf("Changing File, current File Size %zu\n", this->current_file_size);
-	fclose(this->logfile);
-	this->ignite_compressor();
-	this->filename = mkfile_name(false, &this->filename_len);
-	this->logfile = fopen(this->filename, "w+");
-	assert(this->logfile != nullptr);
-	this->current_file_size = 0;
+	printf("Changing File, current File Size %zu\n", _this->current_file_size);
+	fclose(_this->logfile);
+	_this->file_zip.send_signal(); // Post compressor thread a new Job
+	logger_free(_this->filename);
+	_this->filename = mkfile_name(false, &_this->filename_len);
+	_this->logfile = fopen(_this->filename, "w+");
+	assert(_this->logfile != nullptr);
+	_this->current_file_size = 0;
 }
 
 char *get_current_time_stamp() {
@@ -274,25 +295,25 @@ void *logging_thread(void *args) {
 		.tv_sec = 0
 	};
 
-	while (this->is_running) {
-		while (TAILQ_EMPTY(&GET_PRIVATE(this).head)) {
+	while (_this->is_running) {
+		while (TAILQ_EMPTY(&GET_PRIVATE(_this).head)) {
 			nanosleep(&sleep_time, nullptr);
 		}
 
 		char *ts = get_current_time_stamp();
-		log_info_t *msg = this->pop_front();
-		this->current_file_size +=
-			fprintf(this->logfile, "%s [%lu] %s : %s:%d > [ %s ]\n", ts, msg->tid, get_log_str(msg->level),
+		log_info_t *msg = _this->pop_front();
+		_this->current_file_size +=
+			fprintf(_this->logfile, "%s [%lu] %s : %s:%d > [ %s ]\n", ts, msg->tid, get_log_str(msg->level),
 			        msg->func,
 			        msg->line, msg->log_line);
-		fflush(this->logfile);
+		fflush(_this->logfile);
 		logger_free(msg->func);
 		logger_free(msg->log_line);
 		logger_free(msg);
 		logger_free(ts);
 
-		if (this->current_file_size >= this->max_file_size) {
-			this->logger->cycle_file();
+		if (_this->current_file_size >= _this->max_file_size) {
+			_this->logger->cycle_file();
 		}
 	}
 	return nullptr;
@@ -312,7 +333,7 @@ bool do_compress(char *in, char *out) {
 
 	return_if(!infile || !outfile, false);
 
-	char inbuffer[KB(4)];
+	char inbuffer[KB(4)] = {0};
 	size_t num_read;
 	size_t total_read = 0;
 	while ((num_read = fread(inbuffer, 1, sizeof(inbuffer), infile)) > 0) {
@@ -326,31 +347,68 @@ bool do_compress(char *in, char *out) {
 	       total_read,
 	       file_size(out),
 	       (1.0 - file_size(out) * 1.0 / total_read) * 100.0);
+
+	return true;
 }
 
-void *compress_log_file(void *args) {
-	compressor_params_t *params = args;
-	size_t len = strnlen(params->orig_filename, params->filename_len) + 3;
-	params->compressed_filename = calloc(len, sizeof(char));
-	snprintf(params->compressed_filename, len, "%s.z", params->orig_filename);
-	do_compress(params->orig_filename, params->compressed_filename);
-
-	logger_free(params->compressed_filename);
-	logger_free(params->orig_filename);
-	logger_free(params);
-	pthread_exit(nullptr);
+void* zip_file_routine(void *args) {
+	(void)args;
+	zip_all_uncompressed_files();
+	while (_this->is_running) {
+		_this->file_zip.wait_signal();
+		zip_all_uncompressed_files();
+	}
+	return nullptr;
 }
 
-bool ignite_compressor_routine(void) {
-	pthread_spin_lock(&this->workerLock);
-	compressor_params_t *params = calloc(1, sizeof(compressor_params_t));
-	params->file_path = this->file_path;
-	params->orig_filename = this->filename;
-	params->filename_len = this->filename_len;
-	this->filename = nullptr;
+void zip_all_uncompressed_files(void) {
+	DIR *cwd = nullptr;
+	struct dirent *dir = nullptr;
 
-	pthread_t worker;
-	pthread_create(&worker, nullptr, &compress_log_file, params);
-	pthread_detach(worker);
-	pthread_spin_unlock(&this->workerLock);
+	cwd = opendir(_this->file_path);
+	assert(cwd != nullptr);
+	while ((dir = readdir(cwd)) != nullptr)
+	{
+		char *file = strdup(dir->d_name);
+		if ((file[0] == '.') || (strstr(file, ".z") != nullptr) || (strncmp(file, _this->filename, _this->filename_len) == 0)) {
+			logger_free(file);
+			continue;
+		} else {
+			size_t len = strlen(file) + 3;
+			char *compressed_filename = calloc(len, sizeof(char));
+			snprintf(compressed_filename, len, "%s.z", file);
+			do_compress(file, compressed_filename);
+			printf("Original file %s, New File Name: %s\n", file, compressed_filename);
+			logger_free(compressed_filename);
+		}
+		logger_free(file);
+	}
+	closedir(cwd);
 }
+
+//void *compress_log_file(void *args) {
+//	compressor_params_t *params = args;
+//	size_t len = strnlen(params->orig_filename, params->filename_len) + 3;
+//	params->compressed_filename = calloc(len, sizeof(char));
+//	snprintf(params->compressed_filename, len, "%s.z", params->orig_filename);
+//	do_compress(params->orig_filename, params->compressed_filename);
+//
+//	logger_free(params->compressed_filename);
+//	logger_free(params->orig_filename);
+//	logger_free(params);
+//	pthread_exit(nullptr);
+//}
+
+//bool ignite_compressor_routine(void) {
+//	pthread_spin_lock(&_this->workerLock);
+//	compressor_params_t *params = calloc(1, sizeof(compressor_params_t));
+//	params->file_path = _this->file_path;
+//	params->orig_filename = _this->filename;
+//	params->filename_len = _this->filename_len;
+//	_this->filename = nullptr;
+//
+//	pthread_t worker;
+//	pthread_create(&worker, nullptr, &compress_log_file, params);
+//	pthread_detach(worker);
+//	pthread_spin_unlock(&_this->workerLock);
+//}
--- test/main.c
+++ test/main.c
@@ -4,9 +4,12 @@
 
 int main(int argc, char **argv) {
 
-	assert(argc > 1);
-	create_logger("/home/khubaibumer/logs");
-	set_max_file_size(KB(3));
+	(void)argc;
+	(void)argv;
+	const char *file = "/mnt/c/Users/khubaib.umer/git/MemPool/include/MemPool.h";
+//	assert(argc > 1);
+	create_logger("Test", "/home/khubaibumer/logs");
+	set_max_file_size(KB(1));
 
 	get_logger()->trace(__func__, __LINE__, "%s\n", "powwow");
 
@@ -14,7 +17,7 @@ int main(int argc, char **argv) {
 
 	char inbuffer[KB(4)];
 	size_t num_read;
-	FILE *ptr = fopen(argv[1], "r");
+	FILE *ptr = fopen(file, "r");
 	assert(ptr != NULL);
 	while ((num_read = fread(inbuffer, 1, sizeof(inbuffer), ptr)) > 0) {
 		INFO("%s", inbuffer);
